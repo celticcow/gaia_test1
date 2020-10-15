c20ff9a5f02783c6e70a3f904d16cf4149a90a2b
Celticcow <gregory.dunlap@gmail.com>
2020-10-14 20:59:40 -0500

    api explore and example for later

--- gaia1.py
+++ gaia1.py
@@ -35,15 +35,9 @@ payload to login to CMA
 returns Session ID (SID)
 """
 def login(user, password, gateway):
-    debug = 0
-
     payload = {"user" : user, "password" : password}
     response = api_call(gateway, "login", payload, "")
 
-    if(debug == 1):
-        print("---------------------------------------")
-        print(json.dumps(response))
-        print("---------------------------------------")
     return response["sid"]
 
 def main():
@@ -55,6 +49,63 @@ def main():
 
     print(sid)
 
+    show_route_response = api_call(gw, "show-routes-static", {}, sid)
+
+    print(json.dumps(show_route_response))
+
+    show_cluster_state_response = api_call(gw, "show-cluster-state", {}, sid)
+
+    print(json.dumps(show_cluster_state_response))
+
+    show_radius_response = api_call(gw, "show-radius", {}, sid)
+
+    print(json.dumps(show_radius_response))
+
+    show_asset_response = api_call(gw, "show-asset", {}, sid)
+
+    print(json.dumps(show_asset_response))
+
+    print("#############################################################")
+    power_supply_len = len(show_asset_response["power-supply"])
+
+    for i in range(power_supply_len):
+        print(show_asset_response['power-supply'][i])
+
+    disk_len = len(show_asset_response["disk"])
+
+    for i in range(disk_len):
+        print(show_asset_response['disk'][i])
+
+    system_len = len(show_asset_response["system"])
+
+    for i in range(disk_len):
+        print(show_asset_response['system'][i])
+    
+    memory_len = len(show_asset_response["memory"])
+
+    for i in range(disk_len):
+        print(show_asset_response['memory'][i])
+
+    print("#############################################################")
+    show_diag_json = {"category": "os", "topic": "cpu"}
+    show_diag_response = api_call(gw, "show-diagnostics", show_diag_json, sid)
+
+    print(json.dumps(show_diag_response))
+
+    for i in range(show_diag_response['total']):
+        print("CPU " + str(i))
+        print(show_diag_response['objects'][i]['idle'], end=' ')
+        print("idle")
+        print(show_diag_response['objects'][i]['io-wait'], end=' ')
+        print("io-wait")
+        print(show_diag_response['objects'][i]['system'], end=' ')
+        print("system")
+        print(show_diag_response['objects'][i]['user'], end=' ')
+        print("user")
+
+
+
+    print("#############################################################")
     logout = api_call(gw, "logout", {}, sid)
 
     print(json.dumps(logout))
